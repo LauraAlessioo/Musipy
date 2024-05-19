0d20a8d194442323daf7eff3d79d118fd8f872de
LauraAlessioo <laura.carvalho@sptech.school>
2024-05-19 12:12:28 -0300

    integração

---
+++ ProjetoIndividual/src/database/script-tabelas.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE  musipy;
+
+use musipy;
+
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+select * from usuario;	
+
+truncate table usuario;
